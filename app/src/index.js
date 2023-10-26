33a8d4950f4b6c1e77adc55c35bd06a1181e4004
kip2 <tqa1no4@gmail.com>
2023-10-27 01:11:35 +0900

    add: skelton作成

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
-import Hello from "./components/Hello";
+import App from "./components/App";
 
-render(<Hello />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
