0032c094daf1cbf51788e7254675a6f7bfa52d27
kip2 <tqa1no4@gmail.com>
2023-10-27 22:01:13 +0900

    レイアウトの位置を変更

--- app/src/components/Category.js
+++ app/src/components/Category.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Category({ category }) {
     return (
         <>
-            <h1>{category}</h1>
+            <p>{category}</p>
         </>
     );
 }
