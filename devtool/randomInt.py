9bf4476cb6ef7f3ac63ce2c210d254250d2bc5fe
kip2 <tqa1no4@gmail.com>
2023-10-27 22:43:31 +0900

    randomな値ジェネレータ

---
+++ devtool/randomInt.py
@@ -0,0 +1,11 @@
+import random
+
+def get_random_int(min, max):
+    print(random.randint(min, max))
+
+def generation_random_price(count, min, max):
+    for i in range(count):
+        get_random_int(min, max)
+
+if __name__ == "__main__":
+    generation_random_price(5, 8000, 20000)
