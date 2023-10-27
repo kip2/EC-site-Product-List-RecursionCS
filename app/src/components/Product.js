0b5a256b7b0b0d88c8861e33003096616ebdb5cf
kip2 <tqa1no4@gmail.com>
2023-10-28 00:03:59 +0900

    レイアウト調整

--- app/src/components/Product.js
+++ app/src/components/Product.js
@@ -1,13 +1,16 @@
 import React from "react";
 import sytles from "../css/styles.css";
 
-export default function Product({ name, imgUrl, price }) {
+export default function Product({ name, imgUrl, price, description }) {
     return (
         <>
             <section className={sytles.card}>
                 <img src={imgUrl} />
                 <p>{name}</p>
                 <p>¥{price}</p>
+                <div className={sytles.popup} hidden>
+                    {description}
+                </div>
             </section>
         </>
     );
