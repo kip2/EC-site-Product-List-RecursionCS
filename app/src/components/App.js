3819f63515e9370af9d5cd5a7803927d0ee39378
kip2 <tqa1no4@gmail.com>
2023-10-27 21:47:09 +0900

    配列の内容を追加

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -3,10 +3,107 @@ import sytles from "../css/styles.css";
 import Category from "./Category";
 
 const categories = [
-		"test1",
-		"test2",
-		"test3"
-	];
+    {
+        "category": "Desktop PC",
+        "products": [
+            { 
+                "name": "DESKTOP1",
+                "imgUrl": "./img/desktop1.png",
+                "price": 12345
+            },
+            { 
+                "name": "DESKTOP1",
+                "imgUrl": "./img/desktop2.png",
+                "price": 12345
+            },
+            { 
+                "name": "DESKTOP1",
+                "imgUrl": "./img/desktop3.png",
+                "price": 12345
+            },
+            { 
+                "name": "DESKTOP1",
+                "imgUrl": "./img/desktop4.png",
+                "price": 12345
+            }
+        ]
+    },
+    {
+        "category": "Laptop PC",
+        "products": [
+            { 
+                "name": "LAPTOP1",
+                "imgUrl": "./img/laptop1.png",
+                "price": 12345
+            },
+            { 
+                "name": "LAPTOP1",
+                "imgUrl": "./img/laptop2.png",
+                "price": 12345
+            },
+            { 
+                "name": "LAPTOP1",
+                "imgUrl": "./img/laptop3.png",
+                "price": 12345
+            },
+            { 
+                "name": "LAPTOP1",
+                "imgUrl": "./img/laptop4.png",
+                "price": 12345
+            }
+        ]
+    },
+    {
+        "category": "Tablet PC",
+        "products": [
+            { 
+                "name": "TABLET1",
+                "imgUrl": "./img/tablet1.png",
+                "price": 12345
+            },
+            { 
+                "name": "TABLET1",
+                "imgUrl": "./img/tablet2.png",
+                "price": 12345
+            },
+            { 
+                "name": "TABLET1",
+                "imgUrl": "./img/tablet3.png",
+                "price": 12345
+            },
+            { 
+                "name": "TABLET1",
+                "imgUrl": "./img/tablet4.png",
+                "price": 12345
+            },
+            { 
+                "name": "TABLET1",
+                "imgUrl": "./img/tablet5.png",
+                "price": 12345
+            }
+        ]
+    },
+    {
+        "category": "Smart Phone",
+        "products": [
+            { 
+                "name": "PHONE1",
+                "imgUrl": "./img/phone1.png",
+                "price": 12345
+            },
+            { 
+                "name": "PHONE1",
+                "imgUrl": "./img/phone1.png",
+                "price": 12345
+            },
+            { 
+                "name": "PHONE1",
+                "imgUrl": "./img/phone1.png",
+                "price": 12345
+            }
+        ]
+    }
+]
 
 export default function App() {
 
@@ -14,7 +111,7 @@ export default function App() {
 		<div>
 			{
 				categories.map(category => {
-					return <Category category={category} />;
+					return <Category category={category.category} />;
 				})
 			}
 		</div>
