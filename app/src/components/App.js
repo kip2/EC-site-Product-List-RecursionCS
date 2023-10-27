e63fc32e92aa774919664d2fdfbcc988cddd5c96
kip2 <tqa1no4@gmail.com>
2023-10-27 21:40:31 +0900

    定数を外部化

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import sytles from "../css/styles.css";
 import Category from "./Category";
 
-
-export default function App() {
-	const categories = [
+const categories = [
 		"test1",
 		"test2",
 		"test3"
-	]
+	];
+
+export default function App() {
 
 	return (
 		<div>
